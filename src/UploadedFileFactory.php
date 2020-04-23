cf79533c741d0c1c7e013434f98017bb54582911
Vaibhav Pandey <contact@vaibhavpandey.com>
2020-04-23 16:42:42 +0530

    Methods for creating UploadedFile from $_FILES.

--- src/UploadedFileFactory.php
+++ src/UploadedFileFactory.php
@@ -11,6 +11,7 @@
 
 namespace Sandesh;
 
+use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\UploadedFileFactoryInterface;
 use Psr\Http\Message\UploadedFileInterface;
@@ -33,5 +34,54 @@ class UploadedFileFactory implements UploadedFileFactoryInterface
     ): UploadedFileInterface
     {
         return new UploadedFile($stream, (int)$size, (int)$error, $clientFilename, $clientMediaType);
+    }
+
+    /**
+     * @param array $files
+     * @return UploadedFileInterface[]
+     */
+    public function createUploadedFiles(array $files)
+    {
+        $factory = new StreamFactory();
+        $list = [];
+        foreach ($files as $key => $data) {
+            if (is_array($data)) {
+                if (isset($data['tmp_name'])) {
+                    $list[$key] = $this->getUploadedFile($factory, $data);
+                } else {
+                    $list[$key] = $this->createUploadedFiles($data);
+                }
+            }
+        }
+        return $list;
+    }
+
+    /**
+     * @param StreamFactoryInterface $factory
+     * @param array $file
+     * @return UploadedFileInterface|UploadedFileInterface[]
+     */
+    protected function getUploadedFile(StreamFactoryInterface $factory, array $file)
+    {
+        if (is_array($file['tmp_name'])) {
+            $files = [];
+            foreach (array_keys($file['tmp_name']) as $key) {
+                $files[$key] = new UploadedFile(
+                    $factory->createStreamFromFile($file['tmp_name'][$key]),
+                    (int)$file['size'][$key],
+                    (int)$file['error'][$key],
+                    $file['name'][$key],
+                    $file['type'][$key]
+                );
+            }
+            return $files;
+        }
+        return $this->createUploadedFile(
+            $factory->createStreamFromFile($file['tmp_name']),
+            (int)$file['size'],
+            (int)$file['error'],
+            $file['name'],
+            $file['type']
+        );
     }
 }
